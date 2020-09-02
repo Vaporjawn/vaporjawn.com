9dd3c7340e4a80175c0b8e35c7fbe852a46bc8e3
Victor Williams <vrwilliams719@gmail.com>
2020-09-02 19:17:40 -0400

    the loading screen finally works
    but it needs to be claeaned up

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,34 @@
+//BACKGROUND SELECTION
 function initBackground(check){
     if(check == undefined){
-        check = initBackgrounds[Math.floor(Math.random() * 0)+1];
+        check = Math.floor(Math.random() * 0)+1;
     }
     else{
-        check = initBackgrounds[check];
+        check = check;
     }
     var link = "./assets/videos/";
     link = link + check + ".mp4";
     document.getElementById("source").src = link;
     console.log(link);
 }
+//LOADING SCREEN
+
+function isLoaded(){
+    document.getElementById('preloader').remove();
+    document.getElementById('realBody').style.display = "block";
+    document.getElementById('fullscreen-bg').style.display = "block";
+
+}
+
+window.onload = setTimeout(function () {
+    document.getElementById('body').style.display = "block";
+    document.getElementById('preloader').style.display = "block";
+    $('body').toggleClass('body');
+    isLoaded();},5000);
+
+// JQUERY IMPORT
+
+var script = document.createElement('script');
+script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
