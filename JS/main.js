d3cf3412b8b043808ae387d0d6e68cd717c94b77
Victor Williams <vrwilliams719@gmail.com>
2020-09-05 02:42:38 -0400

    need to try to work on include

--- JS/main.js
+++ JS/main.js
@@ -25,10 +25,12 @@ window.onload = setTimeout(function () {
     document.getElementById('preloader').style.display = "block";
     $('body').toggleClass('body');
     isLoaded();},3000);
-
-// JQUERY IMPORT
-
-var script = document.createElement('script');
-script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
-script.type = 'text/javascript';
-document.getElementsByTagName('head')[0].appendChild(script);
+    function supportsImports() {
+        return 'import' in document.createElement('link');
+      }
+      
+      if (supportsImports()) {
+        // Good to go!
+      } else {
+      
+}
