871dbe3562d7b4a99ecc3b888f16abe7b7a27ca2
Victor Williams <vrwilliams719@gmail.com>
2020-09-07 22:28:18 -0400

    asset request

--- JS/main.js
+++ JS/main.js
@@ -10,3 +10,36 @@ function initBackground(check){
     document.getElementById("source").src = link;
     console.log(link);
 }
+//LOADING SCREEN
+
+function isLoaded(){
+    document.getElementById('preloader').remove();
+    document.getElementById('realBody').style.display = "block";
+    document.getElementById('fullscreen-bg').style.display = "block";
+
+}
+
+window.onload = setTimeout(function () {
+    document.getElementById('body').style.display = "block";
+    document.getElementById('preloader').style.display = "block";
+    $('body').toggleClass('body');
+    isLoaded();},3000);
+    function supportsImports() {
+        return 'import' in document.createElement('link');
+      }
+      
+      if (supportsImports()) {
+        // Good to go!
+      } else {
+      
+}
+
+ function assestRequest() {
+     var asset;
+    $.get("https://hook.integromat.com/n4eo7wthq88it9lipndvt61iykl73lfq",
+    function(data, status){
+      alert("Data: " + data + "\nStatus: " + status);
+      asset = data;
+    });
+    return asset;
+  };
